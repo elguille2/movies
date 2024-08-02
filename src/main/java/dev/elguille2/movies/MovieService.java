d32f3ed7beb4f00a800915057fbdcdcdd2bc17be
elguille2 <guillermo2pacheco@gmail.com>
2024-08-02 18:51:57 +0200

    feat: add Movie and Review entities with API to fetch all movies
    - Created Movie and Review entities
    
    - Implemented MovieController, MovieService and MovieRepository
    
    Added functionality to fetch all movies via API

---
+++ src/main/java/dev/elguille2/movies/MovieService.java
@@ -0,0 +1,18 @@
+package dev.elguille2.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getMovies(){
+        return movieRepository.findAll();
+    }
+
+}
