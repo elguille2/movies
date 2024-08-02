ed672658a0c7f5e9631595b7e05f23c4a7a4581a
elguille2 <guillermo2pacheco@gmail.com>
2024-08-02 20:28:41 +0200

    feat: add functionality to fetch movie by imdbId

--- src/main/java/dev/elguille2/movies/MovieService.java
+++ src/main/java/dev/elguille2/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.elguille2.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,4 +17,7 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public Optional<Movie> getMovieByImdbId(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
 }
